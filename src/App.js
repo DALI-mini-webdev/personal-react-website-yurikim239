5b52f9cbba4d91383a2b11215432a61d061354c5
yurikim239 <ykim@bates.edu>
2021-01-30 17:04:28 -0500

    Transfer to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import computer from './Images/pixelcomputer.png';
 
 function App() {
+  const buttonFunction =() => {
+    console.log("Someone said hi to console!")
+  }
+  const onChangeFunction = (event) => {
+    console.log(event.target.value);
+  }
+  const instrumentsList = ["clarinet.", "piano."];
+  const instrumentsMap = instrumentsList.map((instruments) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>An instrument I can sort of play is the {instruments}</p>
+  )
+  });
+  return (
+    <div className="App bgimage">
+   
+    <div>
+      <h1 className="red-text orange-bg">Hi! My name is Yuri.</h1>
+        <img src={computer}></img>
+    </div>
+
+    <div>
+      <button onClick={buttonFunction}>Click me to say hi to console!</button>
+    </div>
+
+    <div>
+      <h2 className="red-text yellow-bg">Here are some fun facts about me!</h2>   
+            <ul className="red-text">
+              <li>I like to do a lot of art in my free time.</li>
+              <li>My favorite historical figure is Juan Pujol García.</li>
+              <li>I'm really craving cheesecake right now.</li>
+              <li>I can't think of anything else.</li>
+            </ul>
+    </div>
+
+    <div>{instrumentsMap}</div>
+
+    <div>
+        <h2 className="red-text green-bg">Storytime:</h2>
+        <p className="red-text">One time my cat escaped my house and I spent over 2 hours trying to chase and catch him.<br></br>I had to crawl underneath cars, go into backyards, and basically make a fool of myself the entire time.<br></br>And after all that running around, he literally just walked back in the house.<br></br>...I got him back, but at what cost?</p>
+    </div>
+
+    <div>
+        <h2 className="red-text blue-bg">This is my favorite website right now.</h2>
+        <a className="App-link" href="http://colormind.io/"> Colormind generates really cool palettes for you!</a>  
+        <p className="red-text">Tell console what you think of it!</p>
+    </div>
+
+    <div>
+     <input type='text' onChange={onChangeFunction}/>
+    </div>
     </div>
   );
-}
+  }
 
 export default App;
